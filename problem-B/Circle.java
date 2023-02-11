e47ee8abddb9a61efdaee3a4b1f9345e07c28469
mukhamedjan140 <124435839+220103140@users.noreply.github.com>
2023-02-11 20:14:10 +0600

    Circle.java

--- problem-B/Circle.java
+++ problem-B/Circle.java
@@ -1,15 +1,20 @@
-public class Circle extends Shape{
-    private double radius = 1.0;
-    public Circle(){
+public class Circle extends Shape
+{
+    private double radius;
 
+    public Circle()
+    {
     }
-    public Circle(double radius){
-        this.radius=radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
     }
-    public Circle(double radius, String color, boolean filled){
-        this.radius=radius;
-        super.setColor(color);
-        super.setFilled(filled);
+
+    public Circle(double radius, String color, boolean filled)
+    {
+        super(color, filled);
+        this.radius = radius;
     }
 
     public double getRadius() {
@@ -19,15 +24,19 @@ public class Circle extends Shape{
     public void setRadius(double radius) {
         this.radius = radius;
     }
-    public double getArea(){
-        return Math.PI*getRadius()*getRadius();
-    }
-    public double getPerimeter(){
-        return 2*Math.PI*getRadius();
-    }
-    @Override
-    public String toString(){
-        return "Circle[Shape[color = " + getColor() + " filled = " + isFilled() +" ] radius = " + getRadius();
+
+    public double getArea()
+    {
+        return Math.pow(radius, 2) * Math.PI;
     }
 
+    public double getPerimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Circle[%s], radius = %s]", super.toString(), radius);
+    }
 }
